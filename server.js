860c6615e205ef4bf7ea6fd547ccaeb831d7f1ff
Ganesh kumar <ganesh104012@gmail.com>
2016-10-07 07:08:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 app.get('/name',function(req,res){
    //res.send('Ganesh is my name!');
-   res.sendFile(path.join(__dirname,'ui','index.html'));
+   res.sendFile(path.join(__dirname,'ui','name.html'));
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
